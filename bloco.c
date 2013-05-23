d282e94cc760a302db2922963bc6022b838ed47d
Roffelos <andreacciolyl@hotmail.com>
2013-05-23 16:41:02 -0300

    Adicionei TUTO ds broco

--- bloco.c
+++ bloco.c
@@ -2,14 +2,17 @@
 
 void mostra_bloco(Bloco* b)
 {
-
-
-
+	if(b->tipo == VISIVEL)
+		mvaddstr(b->y, b->x, "[]");
 }
 
-Bloco novo_bloco(Bloco*)
+
+Bloco novo_bloco(it x, int y, BlocoTipo bTipo)
 {
+	Bloco bloco;
+	bloco.x = x;
+	bloco.y = y;
+	bloco.tipo = bTipo;
 
-
-
+	retun bloco;
 }
