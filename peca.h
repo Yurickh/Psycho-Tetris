afca10a67e65b90bfe3df7a42b51e2a7f920e5ea
Yurickh <yurick.hauschild@gmail.com>
2013-05-23 10:45:29 -0300

    peca.h implementado

---
+++ peca.h
@@ -0,0 +1,23 @@
+#ifndef PECA_H
+#define PECA_H
+
+#ifdef SERV_PECA
+#define EXT_MOD_PECA extern
+#else
+#define EXT_MOD_PECA
+#endif
+
+typedef struct
+{
+	unsigned int x;
+	unsigned int y;
+	Bloco bloco[4];
+} Peca;
+
+Peca nova_peca(Tela* tela, int x, int y);
+void peca_move_x(Peca* peca, int x);
+void peca_move_y(Peca* peca, int y);
+void mostra_peca(Peca* p);
+void prende_peca(Peca* p, Tela* t);
+
+#endif
