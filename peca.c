c0b82dfcf977a570b45fbcf955798889e4875696
Yurickh <yurick.hauschild@gmail.com>
2013-05-23 14:20:31 -0300

    Peca.c implementado. Peca.h remodelado.

---
+++ peca.c
@@ -0,0 +1,63 @@
+#include <time.h>
+#include <stdlib.h>
+#include "bloco.h"
+#include "peca.h"
+
+#define NIL -1
+
+void atualiza_blocos(Peca* peca, int x=NIL, int y=NIL)
+{
+	switch(peca->tipo)
+	{
+		case QUADRADO:
+			if(x!=NIL)
+			{
+				peca->bloco[0].x = x+1;
+				peca->bloco[1].x = x+2;
+				peca->bloco[2].x = x+1;
+				peca->bloco[3].x = x+2;
+			}
+			if(y!=NIL)
+			{
+				peca->bloco[0].y = y+1;
+				peca->bloco[1].y = y+1;
+				peca->bloco[2].y = y+2;
+				peca->bloco[3].y = y+2;
+			}
+		break;
+		default:
+			exit(1);
+	}
+}
+
+Peca nova_peca (Tela* tela, int x, int y)
+{
+	Peca peca;
+
+	peca.x = x;
+	peca.y = y;
+
+	for(int i=0;i<4;i++) 
+		peca.bloco = novo_bloco(0, 0, INVISIVEL);
+
+	srand(time(NULL));
+	peca.tipo = rand() % TOT_PECA_TIPOS;
+
+	atualiza_blocos(&peca, x, y);
+	
+	return peca;
+}
+
+void peca_move_x (Peca* peca, int x)
+{
+	peca->x = x;
+	atualiza_blocos(peca, x, NIL);
+}
+
+void peca_move_y (Peca* peca, int y)
+{
+	peca->y = y;
+	atualiza_blocos(peca, NIL, y);
+}
+
+
--- peca.h
+++ peca.h
@@ -7,17 +7,29 @@
 #define EXT_MOD_PECA
 #endif
 
+#define TOT_PECA_TIPOS 1
+#define QUADRADO 0
+
+/**Estrutura básica de um peça.
+Cada peça deve conter suas próprias coordenadas e as coordenadas de cada um de seus blocos.
+*/
 typedef struct
 {
-	unsigned int x;
-	unsigned int y;
+	int x;
+	int y;
+	int tipo;
 	Bloco bloco[4];
 } Peca;
 
+/// Cria uma nova peça da forma quadrada.
 Peca nova_peca(Tela* tela, int x, int y);
+/// Move a peça para a posição x.
 void peca_move_x(Peca* peca, int x);
+/// Mova a peça para a posição y.
 void peca_move_y(Peca* peca, int y);
+/// Mostra a peça na tela.
 void mostra_peca(Peca* p);
+/// Prende a peça no fim da tela.
 void prende_peca(Peca* p, Tela* t);
 
 #endif
