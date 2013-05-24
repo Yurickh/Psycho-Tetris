33ee5a06a85a2e86b3786c5991a4b53ea274f393
Roffelos <andreacciolyl@hotmail.com>
2013-05-24 10:34:30 -0300

    Added o engine, mas n entendi muito bem a funcao pega_input e precisa verificar qual sera o tamanho da tela

--- engine.c
+++ engine.c
@@ -1,6 +1,21 @@
+#define DIREITA 261
+#define BAIXO 258
+#define ESQUERDA 260
+#define PRENDE 10  /*um enter*/
+#define ROTACIONA 'r'
+
 void inicializa_ncurses()
 {
+	int max_y, max_x;
+
 	initscr();
+
+	getmaxyx(stdscr, max_y, max_x);
+	if ((max_y != 24) || (max_x != 80)) {
+		endwin();
+		printf("Redimensione o terminal para 80 colunas por 24 linhas.\n");
+		exit(0);
+	}
 	cbreak();
 	noecho();
 	timeout(0);
@@ -10,4 +25,17 @@ void inicializa_ncurses()
 void finaliza_ncurses()
 {
 	endwin();
+}
+
+int pega_input(int input)
+{
+	switch (input)
+	{
+		case DIREITA: return 1 ; break;
+		case ESQUERDA: return 2 ; break;
+		case BAIXO: return 3 ; break;
+		case ROTACIONA: return 4 ; break;
+		case PRENDE: return 5 ; break;
+	}
+	return 0;
 }
