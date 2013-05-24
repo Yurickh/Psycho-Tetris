74b143c67f5524420bbb0f9fa1dc43ae40a695db
Yurickh <yurick.hauschild@gmail.com>
2013-05-24 13:07:10 -0300

    Adicionado engine.c

---
+++ engine.c
@@ -0,0 +1,41 @@
+#define PRENDE 10  /*um enter*/
+#define CIMA		259
+#define BAIXO		258
+#define ESQUERDA	260
+#define DIREITA		261
+
+void inicializa_ncurses()
+{
+	int max_y, max_x;
+
+	initscr();
+
+	getmaxyx(stdscr, max_y, max_x);
+	if ((max_y != 24) || (max_x != 80)) {
+		endwin();
+		printf("Redimensione o terminal para 80 colunas por 24 linhas.\n");
+		exit(0);
+	}
+	cbreak();
+	noecho();
+	timeout(0);
+	curs_set(0);
+}
+
+void finaliza_ncurses()
+{
+	endwin();
+}
+
+int pega_input(int input)
+{
+	switch (input)
+	{
+		case DIREITA: return KEY_RIGHT ; break;
+		case ESQUERDA: return KEY_LEFT ; break;
+		case BAIXO: return KEY_DOWN ; break;
+		case CIMA: return KEY_UP ; break;
+		case PRENDE: return PRENDE ; break;
+	}
+	return 0;
+}
